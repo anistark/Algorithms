c9f03b58b47dafe866fcde09b6d2552aa4401519
Kumar Anirudha <anirudhastark@yahoo.com>
2014-08-20 21:41:44 +0530

    deletedub updated

--- DS/array/deletedub.c
+++ DS/array/deletedub.c
@@ -1,9 +1,11 @@
-/* Remove Duplicate strings from a user input string or integer. */
+/* Remove Duplicate strings from a user input string. */
 
 #include <stdio.h>
 #include <string.h>
 
-void dupstring(){
+
+int main()
+{
 	char a[100],b[100]={0};
 	scanf("%s",a);
 	int f=strlen(a);
@@ -28,43 +30,5 @@ void dupstring(){
 		printf("%c",b[j]);
 	}
 	printf("\n");
-}
-
-void dupint(){
-	int a[100],b[100]={0};
-	scanf("%s",a);
-	int f=strlen(a);
-	int i,j,k=0,count=0;
-	for(i=0;i<f;i++)
-	{
-		for(j=0;j<i;j++)
-		{
-			if(a[i]==b[j])
-			{
-				count=1;
-			}
-		}
-		if(count==0)
-		{
-			b[k++]=a[i];
-		}
-		count=0;
-	}
-	for(j=0;j<strlen(b);j++)
-	{
-		printf("%c",b[j]);
-	}
-	printf("\n");
-}
-
-int main()
-{
-	int t;
-	printf("Enter 1 for string input or 2 for integer input: ");
-	scanf("%d", &t);
-	if(t==1)
-		dupstring();
-	else if(t==2)
-		dupint();
 	return 0;
 }
